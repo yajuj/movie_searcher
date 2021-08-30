33208d5e473b446dd425f42401a52f50e63648fa
Domnatio <noticethatred@gmail.com>
2021-08-30 15:24:26 +0500

    added contacts page

--- src/pages/contacts-page/contacts.js
+++ src/pages/contacts-page/contacts.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import './contacts.styles.scss'
+
 const Contacts = () => {
   return (
-    <div>
-      
+    <div className='information'>
+      <div class='information__box'>
+        <a href='https://t.me/Tlatoany' target='_blank'>@Tlatoani </a>
+        <i class="fab fa-telegram-plane"></i>
+      </div>
+      <div class='information__box'>
+        <a href="mailto:noticethatred@gmail.com" target='_blank'>noticethatred@gmail.com </a>
+        <i class="fas fa-at"></i>
+      </div>
     </div>
   )
 }
